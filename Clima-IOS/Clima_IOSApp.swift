46644ece13756008bc053435a458290680ce5d57
KeshavSinghal2003 <keshavsinghal2003@gmail.com>
2024-07-09 15:38:23 +0530

    Initial Commit

---
+++ Clima-IOS/Clima_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clima_IOSApp.swift
+//  Clima-IOS
+//
+//  Created by Keshav Singhal on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Clima_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clima-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clima-IOS
+//
+//  Created by Keshav Singhal on 09/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
